b55b37dfb675345025fe64a269ba920c2d83ab6d
Crusher2805 <151662737+Crusher2805@users.noreply.github.com>
2024-10-15 14:25:20 -0600

    Update PracticaProgramada1.java

--- practicaprogramada1/PracticaProgramada1.java
+++ practicaprogramada1/PracticaProgramada1.java
@@ -1,51 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package practicaprogramada1;
 
-/**
- *
- * @author sebas
- */
+
 public class PracticaProgramada1 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        int n = 12; 
-        System.out.println("Los números pares desde 0 hasta " + n + " son:");
-        NumerosPares.pares(n);
+        int num = 12; 
+        System.out.println("Los números pares desde 0 hasta " + num + " son:");
+        NumerosPares.pares(num);
         
         PilaP pila = new PilaP();
 
-        // Agregar elementos a la pila
+        pila.apilar(new DatoC("H"));
+        pila.apilar(new DatoC("E"));
+        pila.apilar(new DatoC("L"));
+        pila.apilar(new DatoC("L"));
         pila.apilar(new DatoC("O"));
-        pila.apilar(new DatoC("T"));
-        pila.apilar(new DatoC("A"));
-        pila.apilar(new DatoC("G"));
 
-        // Imprimir la pila
-        System.out.println("Elementos en la pila:");
+
+        System.out.println("Pila ");
         pila.imprimirPila();
         
        Cola cola = new Cola();
 
-        // Agregar elementos a la cola
-        cola.encolar(4);
-        cola.encolar(6);
-        cola.encolar(8);
-        cola.encolar(2);
         cola.encolar(7);
+        cola.encolar(5);
+        cola.encolar(20);
+        cola.encolar(9);
+        cola.encolar(10);
 
-        System.out.println("Cola original:");
+        System.out.println("Cola sin ordenar ");
         cola.imprimirCola();
 
-        // Ordenar la cola
         cola.ordenarCola();
 
-        System.out.println("Cola ordenada:");
+        System.out.println("Cola ya ordenada ");
         cola.imprimirCola(); 
         
         
