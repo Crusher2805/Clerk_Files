9e9dab4a7c71ec025792f94568a56e080823b541
Crusher2805 <151662737+Crusher2805@users.noreply.github.com>
2024-10-15 14:23:01 -0600

    Update NumerosPares.java

--- practicaprogramada1/NumerosPares.java
+++ practicaprogramada1/NumerosPares.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package practicaprogramada1;
 
-/**
- *
- * @author sebas
- */
+
 public class NumerosPares {
      public static void pares (int num) {
         if (num < 0) {
